62ec274fbe83d577ca76b35468d2b90fe4cbd88d
caojian <caojian@inesadt.com>
2020-01-20 13:55:41 +0800

    add users

--- src/Api/Ocr.php
+++ src/Api/Ocr.php
@@ -154,4 +154,79 @@ class Ocr extends Base
         return false;
     }
 
+    /***
+     * @todo 识别图片文字
+     * @param $token
+     * @param $img_url
+     * @return bool|mixed
+     */
+    public static function printedTextBYUrl($token,$img_url)
+    {
+        $url = "https://api.weixin.qq.com/cv/ocr/comm?img_url=".urlencode($img_url)."&access_token=".$token;
+        $response = Curl::getCurl($url);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode'] == 0){
+            return $result;
+        }
+        return false;
+    }
+
+    /***
+     * @todo 识别图片文字
+     * @param $token
+     * @param $path
+     * @return bool
+     */
+    public static function printedTextByFile($token,$path)
+    {
+        $url = "https://api.weixin.qq.com/cv/ocr/comm?access_token=".$token;
+
+        $data = [
+            'img'=> new \CURLFile($path)
+        ];
+        $response = Curl::uploadCurl($data,$url);
+        if(!$response){
+            return false;
+        }
+        $result =  json_decode($response,true);
+
+        if($result['errcode'] == 0){
+            return $result['number'];
+        }
+        return false;
+    }
+
+    public static function vehicleLicenseWithUrl($token,$img_url)
+    {
+        $url = "https://api.weixin.qq.com/cv/ocr/driving?type=MODE&img_url=".urlencode($img_url)."&access_token=".$token;
+        $response = Curl::getCurl($url);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode'] == 0){
+            return $result;
+        }
+        return false;
+    }
+
+    public static function vehicleLicenseWithFile($token,$path)
+    {
+        $url = "https://api.weixin.qq.com/cv/ocr/driving?type=photo&access_token=".$token;
+        $data = [
+            'img'=> new \CURLFile($path)
+        ];
+        $response = Curl::uploadCurl($data,$url);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode'] == 0){
+            return $result;
+        }
+        return false;
+    }
 }
--- src/Api/User.php
+++ src/Api/User.php
@@ -60,5 +60,92 @@ class User extends Base
         return $result['unionid'];
     }
 
+    public static function creatTag($token,$name)
+    {
+        $url = " https://api.weixin.qq.com/cgi-bin/tags/create?access_token=".$token;
+        $data = [
+            'tag'=>$name
+        ];
+        $response = Curl::postCurl($url,$data);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode']==0){
+            return $result;
+        }
+        return false;
+    }
 
+    public static function getInfo($token,$openid,$lang='zh_CN')
+    {
+        $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$token."&openid=".$openid."&lang=".$lang;
+        $response = Curl::getCurl($url);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode']==0){
+            return $result;
+        }
+        return false;
+    }
+
+    public static function batchGetInfo($token,$openids)
+    {
+        $url = "https://api.weixin.qq.com/cgi-bin/user/info/batchget?access_token=".$token;
+        $users = [];
+        foreach ($openids as $openid)
+        {
+            $users[] = [
+                'openid'=>$openid,
+                'lang'=>'zh_CN'
+            ];
+        }
+        $data = [
+            'user_list'=>$users
+        ];
+        $response = Curl::postCurl($data,$url,5);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode']==0){
+            return $result;
+        }
+        return false;
+    }
+
+    public static function getList($token,$openid='')
+    {
+        if(empty($openid)){
+            $url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=".$token;
+        }else{
+            $url = "https://api.weixin.qq.com/cgi-bin/user/get?access_token=".$token."&next_openid=".$openid;
+        }
+
+        $response = Curl::getCurl($url,5);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode']==0){
+            return $result;
+        }
+        return false;
+    }
+
+    public static function getBlack($token)
+    {
+        $url = "https://api.weixin.qq.com/cgi-bin/tags/members/getblacklist?access_token=".$token;
+        $response = Curl::getCurl($url,5);
+        if(!$response){
+            return false;
+        }
+        $result = json_decode($response,true);
+        if($result['errcode']==0){
+            return $result;
+        }
+        return false;
+    }
 }
